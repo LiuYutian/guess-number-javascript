25ab4c57a3fa2c6b3baba53725b3146226f04ea1
LiuYutian <l709178159@gmail.com>
2015-07-15 11:55:15 +0800

    split change and forEash and for

--- practices/compareNumber.js
+++ practices/compareNumber.js
@@ -5,13 +5,13 @@ function CompareNumber(srcValue) {}
 CompareNumber.prototype.comparison = function(nowValue, srcValue) {
     var a = 0, b = 0;
 
-    for(var i = 0; i < srcValue.length; i++) {
-        if(nowValue[i] === srcValue[i]) {
+    srcValue.split("").forEach(function(value, i) {
+        if(nowValue[i] === value) {
             a++;
         }else if(srcValue.indexOf(nowValue[i]) !== -1) {
             b++;
         }
-    }
+    });
 
     return a+"A"+b+"B";
 }
